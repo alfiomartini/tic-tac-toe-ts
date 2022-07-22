38c70a60cf36dc0a17492de5cc950b20d74f3e37
Alfio Martini <alfio.martini@gmail.com>
2022-07-22 04:15:44 -0300

    rewrite Square as a functional component

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-class Square extends React.Component {
-  render() {
-    const { value, handleClick } = this.props;
-    return (
-      <button className="square" onClick={() => handleClick()}>
-        {value}
-      </button>
-    );
-  }
+function Square(props) {
+  const { value, handleClick } = props;
+  return (
+    <button className="square" onClick={() => handleClick()}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
