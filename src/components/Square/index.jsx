7ce23ca865d6fee96674d8b8fdd600a8eab07837
Alfio Martini <alfio.martini@gmail.com>
2022-07-22 04:18:58 -0300

    define a shorter 'onClick' on Square's button

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Square(props) {
   const { value, handleClick } = props;
   return (
-    <button className="square" onClick={() => handleClick()}>
+    <button className="square" onClick={handleClick}>
       {value}
     </button>
   );
